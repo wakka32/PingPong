064c9707ce27308789d679c4c9a5d27a0532debc
william.grenback <william.grenback@elev.ga.ntig.se>
2019-04-24 11:21:34 +0200

    Pong uppgiften

--- src/Pong.java
+++ src/Pong.java
@@ -7,7 +7,10 @@ import java.io.IOException;
 
 public class Pong extends Canvas {
     BufferedImage img = null;
-
+    int x = 400;
+    int y = 50;
+    int vx =4;
+    int vy =-1;
     public Pong() {
         setSize(800,600);
         JFrame frame = new JFrame("Grafik");
@@ -17,6 +20,7 @@ public class Pong extends Canvas {
         frame.setVisible(true);
         try {
             img = ImageIO.read(new File("Pong.jpg"));
+            img = ImageIO.read(new File("Rödboll.gif"));
         } catch (IOException e) {
             e.printStackTrace();
 
@@ -27,8 +31,29 @@ public class Pong extends Canvas {
     public void paint(Graphics g) {
         drawBox(100,100, g);
         drawBox(300,200,g);
-        g.setColor(new Color(0x5500FFFF));
-        g.drawImage(img,0,0,null);
+        g.setColor(new Color(0x000000));
+        g.fillRect(0,0,800,600);
+
+        g.drawImage(img,x,y,null);
+        x = x + vx;
+        y = y + vy;
+        if (vx > 0 && x>800)
+            vx = -3;
+        if (vx < 0 && x<0)
+            vx= 3;
+        if (vy > 0 && y>600)
+            vy = -1;
+
+
+
+
+        try {
+            Thread.sleep(20);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        repaint();
+
     }
 
     private void drawBox(int x, int y, Graphics g) {
